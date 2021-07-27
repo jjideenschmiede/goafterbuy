3f075a4b9a709c0f4984dd8e4289ab8bbd65621f
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-07-27 12:06:48 +0200

    Add new module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jjideenschmiede/goafterbuy
 
 go 1.16
+
+require golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
---
+++ goafterbuy_test.go
@@ -0,0 +1,14 @@
+//**********************************************************
+//
+// Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
+//
+// This file is part of goafterbuy.
+// All code may be used. Feel free and maybe code something better.
+//
+// Author: Jonas Kwiedor
+//
+//**********************************************************
+
+package goafterbuy
+
+func Te
