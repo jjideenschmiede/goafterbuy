ab81fd5070a2f7ec3ac8bdaf3a2eec12689baa91
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-08-12 09:49:57 +0200

    Change comments

--- products.go
+++ products.go
@@ -1,13 +1,13 @@
-//**********************************************************
+//********************************************************************************************************************//
 //
 // Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
 //
 // This file is part of goafterbuy.
 // All code may be used. Feel free and maybe code something better.
 //
-// Author: Jonas Kwiedor
+// Author: Jonas Kwiedor (aka gowizzard)
 //
-//**********************************************************
+//********************************************************************************************************************//
 
 package goafterbuy
 
