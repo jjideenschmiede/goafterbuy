38462b5fd873fde114d29c494a4cd18f64c254ec
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-11-26 12:59:08 +0100

    Add new function

--- sold_items.go
+++ sold_items.go
@@ -13,14 +13,209 @@ package goafterbuy
 
 import "encoding/xml"
 
+// SoldItemsBody is to send the body data
+type SoldItemsBody struct {
+	Request SoldItemsRequest `xml:"Request"`
+}
+
+type SoldItemsRequest struct {
+	AfterbuyGlobal AfterbuyGlobal      `xml:"AfterbuyGlobal"`
+	DataFilter     SoldItemsDataFilter `xml:"DataFilter"`
+	MaxSoldItems   int                 `xml:"MaxSoldItems"`
+}
+
+type SoldItemsDataFilter struct {
+	Filter []SoldItemsFilter `xml:"Filter"`
+}
+
+type SoldItemsFilter struct {
+	FilterName   string                `xml:"FilterName"`
+	FilterValues SoldItemsFilterValues `xml:"FilterValues"`
+}
+
+type SoldItemsFilterValues struct {
+	DateFrom    string `xml:"DateFrom,omitempty"`
+	DateTo      string `xml:"DateTo,omitempty"`
+	FilterValue string `xml:"FilterValue,omitempty"`
+	ValueFrom   string `xml:"ValueFrom,omitempty"`
+}
+
+// SoldItemsReturn is to decode the xml data
+type SoldItemsReturn struct {
+	XmlName    xml.Name              `xml:"Afterbuy"`
+	CallStatus string                `xml:"CallStatus"`
+	CallName   string                `xml:"CallName"`
+	VersionId  int                   `xml:"VersionID"`
+	Result     SoldItemsReturnResult `xml:"Result"`
+}
+
+type SoldItemsReturnResult struct {
+	XmlName      xml.Name              `xml:"Result"`
+	HasMoreItems int                   `xml:"HasMoreItems"`
+	Orders       SoldItemsReturnOrders `xml:"Orders"`
+	OrdersCount  int                   `xml:"OrdersCount"`
+	LastOrderId  int                   `xml:"LastOrderID"`
+	ItemsCount   int                   `xml:"ItemsCount"`
+}
+
+type SoldItemsReturnOrders struct {
+	XmlName xml.Name               `xml:"Orders"`
+	Order   []SoldItemsReturnOrder `xml:"Order"`
+}
+
+type SoldItemsReturnOrder struct {
+	XmlName                       xml.Name                             `xml:"Order"`
+	InvoiceNumber                 int                                  `xml:"InvoiceNumber"`
+	OrderId                       int                                  `xml:"OrderID"`
+	EbayAccount                   string                               `xml:"EbayAccount"`
+	AmazonAccount                 string                               `xml:"AmazonAccount"`
+	AlternativeItemNumber1        string                               `xml:"AlternativeItemNumber1"`
+	Anr                           int                                  `xml:"Anr"`
+	FeedbackDate                  string                               `xml:"FeedbackDate"`
+	AdditionalInfo                string                               `xml:"AdditionalInfo"`
+	TrackingLink                  string                               `xml:"TrackingLink"`
+	FeedbackLink                  string                               `xml:"FeedbackLink"`
+	OrderDate                     string                               `xml:"OrderDate"`
+	OrderIdAlt                    interface{}                          `xml:"OrderIDAlt"`
+	PaymentInfo                   SoldItemsReturnPaymentInfo           `xml:"PaymentInfo"`
+	IsCheckoutConfirmedByCustomer int                                  `xml:"IsCheckoutConfirmedByCustomer"`
+	BuyerInfo                     SoldItemsReturnBuyerInfo             `xml:"BuyerInfo"`
+	ContainseBayPlusTransaction   bool                                 `xml:"ContainseBayPlusTransaction"`
+	SoldItems                     SoldItemsReturnSoldItems             `xml:"SoldItems"`
+	ShippingInfo                  SoldItemsReturnShippingInfo          `xml:"ShippingInfo"`
+	OrderOriginalCurrency         SoldItemsReturnOrderOriginalCurrency `xml:"OrderOriginalCurrency"`
+}
+
+type SoldItemsReturnPaymentInfo struct {
+	XmlName         xml.Name `xml:"PaymentInfo"`
+	PaymentId       string   `xml:"PaymentID"`
+	PaymentMethod   string   `xml:"PaymentMethod"`
+	PaymentFunction string   `xml:"PaymentFunction"`
+	PaymentDate     string   `xml:"PaymentDate"`
+	AlreadyPaid     string   `xml:"AlreadyPaid"`
+	FullAmount      string   `xml:"FullAmount"`
+	InvoiceDate     string   `xml:"InvoiceDate"`
+}
+
+type SoldItemsReturnBuyerInfo struct {
+	XmlName        xml.Name                      `xml:"BuyerInfo"`
+	BillingAddress SoldItemsReturnBillingAddress `xml:"BillingAddress"`
+}
+
+type SoldItemsReturnBillingAddress struct {
+	XmlName           xml.Name `xml:"BillingAddress"`
+	AfterbuyUserId    int      `xml:"AfterbuyUserID"`
+	AfterbuyUserIdAlt int      `xml:"AfterbuyUserIDAlt"`
+	UserIdPlattform   string   `xml:"UserIDPlattform"`
+	FirstName         string   `xml:"FirstName"`
+	LastName          string   `xml:"LastName"`
+	Title             string   `xml:"Title"`
+	Company           string   `xml:"Company"`
+	Street            string   `xml:"Street"`
+	Street2           string   `xml:"Street2"`
+	PostalCode        string   `xml:"PostalCode"`
+	City              string   `xml:"City"`
+	StateOrProvince   string   `xml:"StateOrProvince"`
+	Country           string   `xml:"Country"`
+	CountryISO        string   `xml:"CountryISO"`
+	Phone             string   `xml:"Phone"`
+	Fax               string   `xml:"Fax"`
+	Mail              string   `xml:"Mail"`
+	IsMerchant        int      `xml:"IsMerchant"`
+	TaxIdNumber       string   `xml:"TaxIDNumber"`
+}
+
+type SoldItemsReturnSoldItems struct {
+	XmlName  xml.Name                `xml:"SoldItems"`
+	SoldItem SoldItemsReturnSoldItem `xml:"SoldItem"`
+}
+
+type SoldItemsReturnSoldItem struct {
+	XmlName                 xml.Name                            `xml:"SoldItem"`
+	ItemDetailsDone         int                                 `xml:"ItemDetailsDone"`
+	ItemId                  int                                 `xml:"ItemID"`
+	AlternativeItemNumber1  string                              `xml:"AlternativeItemNumber1"`
+	AlternativeItemNumber   string                              `xml:"AlternativeItemNumber"`
+	Anr                     int                                 `xml:"Anr"`
+	FulfillmentServiceLevel int                                 `xml:"FulfillmentServiceLevel"`
+	IsAmazonInvoiced        bool                                `xml:"IsAmazonInvoiced"`
+	PlatformSpecificOrderId string                              `xml:"PlatformSpecificOrderId"`
+	EBayTransactiondD       int                                 `xml:"eBayTransactionID"`
+	EBayPlusTransaction     bool                                `xml:"eBayPlusTransaction"`
+	InternalItemType        int                                 `xml:"InternalItemType"`
+	UserDefinedFlag         int                                 `xml:"UserDefinedFlag"`
+	ItemTitle               string                              `xml:"ItemTitle"`
+	ItemQuantity            int                                 `xml:"ItemQuantity"`
+	ItemPrice               string                              `xml:"ItemPrice"`
+	ItemOriginalCurrency    SoldItemsReturnItemOriginalCurrency `xml:"ItemOriginalCurrency"`
+	ItemEndDate             string                              `xml:"ItemEndDate"`
+	TaxRate                 string                              `xml:"TaxRate"`
+	TaxCollectedBy          int                                 `xml:"tax_collected_by"`
+	ItemWeight              string                              `xml:"item_weight"`
+	ItemModDate             string                              `xml:"ItemModDate"`
+	ItemPlatformName        string                              `xml:"ItemPlatformName"`
+	EBayFeedbackCompleted   int                                 `xml:"eBayFeedbackCompleted"`
+	EBayFeedbackReceived    int                                 `xml:"eBayFeedbackReceived"`
+	ShopProductDetails      SoldItemsReturnShopProductDetails   `xml:"ShopProductDetails"`
+}
+
+type SoldItemsReturnItemOriginalCurrency struct {
+	XmlName       xml.Name `xml:"ItemOriginalCurrency"`
+	ItemPrice     string   `xml:"ItemPrice"`
+	ItemPriceCode string   `xml:"ItemPriceCode"`
+	ItemShipping  string   `xml:"ItemShipping"`
+}
+
+type SoldItemsReturnShopProductDetails struct {
+	ProductId       int    `xml:"product_id"`
+	Name            string `xml:"Name"`
+	Ean             string `xml:"EAN"`
+	Anr             int    `xml:"Anr"`
+	BasepriceFactor int    `xml:"BasepriceFactor"`
+}
+
+type SoldItemsReturnShippingInfo struct {
+	XmlName                xml.Name `xml:"ShippingInfo"`
+	ShippingMethod         string   `xml:"ShippingMethod"`
+	ShippingCost           string   `xml:"ShippingCost"`
+	ShippingAdditionalCost string   `xml:"ShippingAdditionalCost"`
+	ShippingTotalCost      string   `xml:"ShippingTotalCost"`
+	ShippingTaxRate        string   `xml:"ShippingTaxRate"`
+	DeliveryDate           string   `xml:"DeliveryDate"`
+}
+
+type SoldItemsReturnOrderOriginalCurrency struct {
+	XmlName                 xml.Name `xml:"OriginalCurrency"`
+	EbayShippingAmount      string   `xml:"EbayShippingAmount"`
+	ShippingAmount          string   `xml:"ShippingAmount"`
+	PaymentSurcharge        string   `xml:"PaymentSurcharge"`
+	PaymentSurchargePerCent string   `xml:"PaymentSurchargePerCent"`
+	InvoiceAmount           string   `xml:"InvoiceAmount"`
+	ExchangeRate            string   `xml:"ExchangeRate"`
+	PayedAmount             string   `xml:"PayedAmount"`
+}
+
 // SoldItemBody is to send the body data
 type SoldItemBody struct {
 	Request SoldItemRequest `xml:"Request"`
 }
 
 type SoldItemRequest struct {
-	AfterbuyGlobal AfterbuyGlobal `xml:"AfterbuyGlobal"`
-	DataFilter     DataFilter     `xml:"DataFilter"`
+	AfterbuyGlobal AfterbuyGlobal     `xml:"AfterbuyGlobal"`
+	DataFilter     SoldItemDataFilter `xml:"DataFilter"`
+}
+
+type SoldItemDataFilter struct {
+	Filter SoldItemDataFilterFilter `xml:"Filter,omitempty"`
+}
+
+type SoldItemDataFilterFilter struct {
+	FilterName   string                         `xml:"FilterName,omitempty"`
+	FilterValues SoldItemDataFilterFilterValues `xml:"FilterValues,omitempty"`
+}
+
+type SoldItemDataFilterFilterValues struct {
+	FilterValue int `xml:"FilterValue,omitempty"`
 }
 
 // SoldItemReturn is to decode the xml data
@@ -266,6 +461,40 @@ type UpdateSoldItemReturnError struct {
 	ErrorCode            int      `xml:"ErrorCode"`
 	ErrorDescription     string   `xml:"ErrorDescription"`
 	ErrorLongDescription string   `xml:"ErrorLongDescription"`
+}
+
+// SoldItems is to get all sold items
+func SoldItems(body SoldItemsBody) (SoldItemsReturn, error) {
+
+	// Convert body
+	convert, err := xml.Marshal(body)
+	if err != nil {
+		return SoldItemsReturn{}, err
+	}
+
+	// Config new request
+	c := Config{nil, convert}
+
+	// Send new request
+	response, err := c.Send()
+	if err != nil {
+		return SoldItemsReturn{}, err
+	}
+
+	// Close request body
+	defer response.Body.Close()
+
+	// Decode data
+	var decode SoldItemsReturn
+
+	err = xml.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return SoldItemsReturn{}, err
+	}
+
+	// Return data
+	return decode, nil
+
 }
 
 // SoldItem is to get a sold item by id
